c65a4623ed93e0193f88efceafea6bcda9826019
michtchakm <michaelmicht@gmail.com>
2013-10-21 08:46:37 -0400

    finished furniture

---
+++ MethodProblems/Ch3Furniture/Furniture.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch3Furniture
+{
+    class Furniture
+    {
+        static void main(string[] args)
+        {
+            string input;
+
+            Console.WriteLine("Type P for Pine, O for Oak, M for Mahogany");
+            input = Console.ReadLine();
+
+            if (input == "P") 
+            {
+                Console.WriteLine("Price is $100");
+            }
+
+            if (input == "O")
+            {
+                Console.WriteLine("Price is $225");
+            }
+
+            if (input == "M")
+            {
+                Console.WriteLine("Price is $310");
+            }
+
+            if (input != "P")
+            {
+                Console.WriteLine("Price is $0");
+            }
+
+            if (input != "O")
+            {
+                Console.WriteLine("Price is $0");
+            }
+
+            if (input != "M")
+            {
+                Console.WriteLine("Price is $0");
+            }
+        }
+    }
+}
---
+++ MethodProblems/MethodProblems/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MethodProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
