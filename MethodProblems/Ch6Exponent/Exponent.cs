3c25ba68c6aef91b893552af5b13558a63488876
michtchakm <michaelmicht@gmail.com>
2013-10-23 08:45:55 -0400

    finished monogram

---
+++ MethodProblems/Ch6Exponent/Exponent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch6Exponent
+{
+    class Exponent
+    {
+        int number;
+
+        public static void SquareNumber(int Number);
+        {
+            Console.WriteLine("{0}", int Number^2)
+        }
+
+        public static void CubeNumber(int Number);
+        {
+            Console.WriteLine("{1}", int Number^3)
+        }
+
+        public static void Main();
+        {
+            Console.WriteLine("Enter an integer")
+            int Number = Console.ReadLine();
+            SquareNumber("
+            CubeNumber("           
+    }
+}
---
+++ MethodProblems/Ch6Monogram/Monogram.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch6Monogram
+{
+    class Monogram
+    {
+        public static void DisplayMonogram(string FirstInitial, string MiddleInitial, string LastInitial)
+        {
+             Console.WriteLine("** {0}. {1}. {2}. **", FirstInitial, MiddleInitial, LastInitial);
+        }
+
+        public static void Main()
+        {
+            DisplayMonogram("T", "I", "N");
+            DisplayMonogram("J", "M", "F");           
+        }
+    }
+}
