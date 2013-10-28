937c8d2c6de5d972865a4ee652fa99a2809ff786
michtchakm <michaelmicht@gmail.com>
2013-10-28 10:07:58 -0400

    started area

---
+++ MethodProblems/Ch6Area/Area.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch6Area
+{
+    class Area
+    {
+        public static void Main();
+        {
+
+        }
+
+    }
+}
---
+++ MethodProblems/Ch6Sum/UsingSum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ch6Sum
+{
+    class UsingSum
+    {
+        public static void Main();
+        {
+            int[] integer = [1] {1};
+            int[] integer = [3] {1, 2, 3};
+            int[] integer = [5] {1, 2, 3, 4, 5};
+            int[] integer = [10] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        }
+
+        public static void Sum();
+        {
+            Console.WriteLine("{0}" int[1] integer);
+            Console.WriteLine("{0}" int[3] integer {1} + int[3] integer {1} + int[3] integer {1});
+            Console.WriteLine("{0}" int[5] integer {1} + int[5] integer {2} + int[5] integer {3} + int[5] integer {4} + interface[5] integer {5});
+            Console.WriteLine("{0}" int[10] integer {1} + int[10] integer {2} + int[10] integer {3} + int[10] integer {4} + interface[10] integer {5} + int[10] integer {6} + int[10] integer {7} + int[10] integer {8} + int[10] integer {9} + int[10] integer {10});
+        }
+    }
+}
